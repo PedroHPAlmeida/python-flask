e50482e55ce1f0997aea1e8bf8ab1b247f854133
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-02-18 00:38:49 -0300

    Aula 03: Criação de um novo jogo

--- jogoteca.py
+++ jogoteca.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 
 class Jogo:
@@ -9,16 +9,33 @@ class Jogo:
         self.console = console
 
 
+tetris = Jogo('Tetris', 'Puzzle', 'Atari')
+gow = Jogo('God of War', 'Rack n Slash', 'PS2')
+mk = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [tetris, gow, mk]
+
+
 app = Flask(__name__)
 
 
-@app.route('/inicio')
-def ola():
-    tetris = Jogo('Tetris', 'Puzzle', 'Atari')
-    gow = Jogo('God of War', 'Rack n Slash', 'PS2')
-    mk = Jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista = [tetris, gow, mk]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+app.run(debug=True)
