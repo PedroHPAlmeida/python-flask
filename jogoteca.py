bff520cdceb9da17bb0f412073ab287813ed4791
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-02-18 00:19:55 -0300

    Aula 02: Listando jogos usando o Flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask import render_template
 
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    tetris = Jogo('Tetris', 'Puzzle', 'Atari')
+    gow = Jogo('God of War', 'Rack n Slash', 'PS2')
+    mk = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [tetris, gow, mk]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 app.run()
